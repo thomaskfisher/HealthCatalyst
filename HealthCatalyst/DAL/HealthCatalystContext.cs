0e241e624131d674bf03e10866bc144e6eb382df
Thomas Fisher <thomas.kimball.fisher@gmail.com>
2016-12-15 20:46:55 -0700

    Added DAL folder and HealthCatalystContext.cs as well as modified the global.asax file

---
+++ HealthCatalyst/DAL/HealthCatalystContext.cs
@@ -0,0 +1,20 @@
+﻿using HealthCatalyst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HealthCatalyst.DAL
+{
+    public class HealthCatalystContext : DbContext
+    {
+        public HealthCatalystContext()
+            : base("HealthCatalystContext")
+        {
+
+        }
+
+        public DbSet<People> Peoples { get; set; }
+    }
+}
--- HealthCatalyst/Global.asax.cs
+++ HealthCatalyst/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HealthCatalyst.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,8 @@ namespace HealthCatalyst
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<HealthCatalystContext>(null);
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
