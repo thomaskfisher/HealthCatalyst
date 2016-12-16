e464cb55ae85ce178b75f8358b8a1fc0adb8aa83
Thomas Fisher <thomas.kimball.fisher@gmail.com>
2016-12-15 20:05:26 -0700

    First Commit

---
+++ HealthCatalyst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HealthCatalyst.Startup))]
+namespace HealthCatalyst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
